dba7c30749004187d5c092bae2f354d510369a26
Nicole Bekkers <nicole@finalist.nl>
2013-10-01 08:42:26 +0200

    Introducing model Quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,12 +1,7 @@
 class QuotesController < ApplicationController
 
-  Quote = Struct.new :text, :author do
-  end
-
   def random
-    @quote = Quote.new(
-      "Don't cry because it's over, smile because it happened.",
-      "Dr. Seuss")
+    @quote = Quote.first
   end
 
 end
--- features/step_definitions/show_quote_steps.rb
+++ features/step_definitions/show_quote_steps.rb
@@ -1,16 +1,17 @@
 
-Given /^there are quotes present in the system$/ do
+Given "there are quotes present in the system" do
+  @quote = Quote.create!(text: Faker::Lorem.sentence,
+                author: Faker::Name.name)
 end
 
-When /^I view the quotes page$/ do
+When "I view the quotes page" do
   visit random_quotes_path
 end
 
-Then /^I see a quote$/ do
-  expect(page).to have_css('.quote',
-              text: "Don't cry because it's over, smile because it happened.")
+Then "I see a quote" do
+  expect(page).to have_css('.quote', text: @quote.text)
 end
 
-And /^I see the author of the quote$/ do
-  expect(page).to have_css('.quote div#author', text: "Dr. Seuss")
+And "I see the author of the quote" do
+  expect(page).to have_css('.quote div#author', text: @quote.author)
 end
