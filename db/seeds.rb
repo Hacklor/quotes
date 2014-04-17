3f3e48ab0a0561ee0423e5f5ef5019cd7338040c
Nicole Bekkers <nicole@finalist.nl>
2014-04-17 13:50:12 +0200

    A few more quotes in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 Quote.create!(author: "Helen Keller", text: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.")
 Quote.create!(author: "Aristotle Onassis", text: "It is during our darkest moments that we must focus to see the light.")
 Quote.create!(author: "Buddha", text: "Health is the greatest gift, contentment the greatest wealth, faithfulness the best relationship.")
+Quote.create!(author: "Albert Einstein", text: "Strive not to be a success, but rather to be of value.")
+Quote.create!(author: "John Lennon", text: "Life is what happens to you while you’re busy making other plans.")
