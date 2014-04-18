59c0c55decdf7b006692a1be0a6a835e4b44d5ea
Nicole Bekkers <nicole@finalist.nl>
2014-04-18 11:50:29 +0200

    Check for error from twitter client

--- app/controllers/backend/twitter_controller.rb
+++ app/controllers/backend/twitter_controller.rb
@@ -5,8 +5,11 @@ module Backend
 
     def tweet
       twitter_service = TwitterService.new
-      twitter_service.tweet(quote.text)
-      flash[:notice] = "The quote has been tweeted"
+      if twitter_service.tweet(quote.text)
+        flash[:notice] = "The quote has been tweeted"
+      else
+        flash[:error] = twitter_service.error_message
+      end
 
       redirect_to backend_quotes_path
     end
--- spec/controllers/backend/twitter_controller_spec.rb
+++ spec/controllers/backend/twitter_controller_spec.rb
@@ -12,7 +12,7 @@ module Backend
       before do
         Quote.stub(:find).and_return(quote)
         TwitterService.stub(:new).and_return(twitter_service)
-        twitter_service.stub(:tweet)
+        twitter_service.stub(:tweet).and_return(true)
       end
 
       it 'finds the quote' do
@@ -20,22 +20,37 @@ module Backend
         post :tweet, quote_id: '1'
       end
 
-      it 'tweets te quote' do
-        twitter_service = double
-        expect(TwitterService).to receive(:new).and_return(twitter_service)
-        expect(twitter_service).to receive(:tweet).with(quote.text)
-
-        post :tweet, quote_id: '1'
-      end
-
-      it 'sets a flash notification' do
-        post :tweet, quote_id: '1'
-        expect(flash[:notice]).to eq("The quote has been tweeted")
-      end
-
       it 'redirects to quotes index' do
         post :tweet, quote_id: '1'
         expect(response).to redirect_to(backend_quotes_path)
+      end
+
+      context 'successfull tweet' do
+
+        it 'tweets the quote' do
+          expect(TwitterService).to receive(:new).and_return(twitter_service)
+          expect(twitter_service).to receive(:tweet).with(quote.text)
+
+          post :tweet, quote_id: '1'
+        end
+
+        it 'sets a flash notification' do
+          post :tweet, quote_id: '1'
+          expect(flash[:notice]).to eq("The quote has been tweeted")
+        end
+
+      end
+
+      context 'an error occurred' do
+
+        it 'sets an error notification' do
+          twitter_service.stub(:tweet).and_return(false)
+          expect(twitter_service).to receive(:error_message).and_return("Status is over 140 characters.")
+
+          post :tweet, quote_id: '1'
+          expect(flash[:error]).to eq("Status is over 140 characters.")
+        end
+
       end
 
     end
