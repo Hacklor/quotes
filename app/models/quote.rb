021adf95db8fd4bbd0e96df37c7044f0460fba51
Nicole Bekkers <nicole@finalist.nl>
2014-04-18 20:42:57 +0200

    Validate total length of a quote in order to tweet it later

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,10 +1,18 @@
 class Quote < ActiveRecord::Base
 
   validates :author, :text, presence: true
+  validate :valid_total_length?
 
   def self.random
     offset = rand(Quote.count)
     Quote.offset(offset).first
+  end
+
+  private
+
+  def valid_total_length?
+    tweet_formatter = TweetFormatter.new(self)
+    tweet_formatter.valid_length?
   end
 
 end
--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -5,7 +5,7 @@ describe Quote do
   it { should validate_presence_of :author }
   it { should validate_presence_of :text }
 
-  describe '#random' do
+  describe '.random' do
 
     it 'returns a random quote' do
       quote1 = Quote.create!(:author => 'Author 1', :text => 'Quote Description 1')
@@ -15,6 +15,19 @@ describe Quote do
       random_quote = Quote.random
 
       expect([quote1, quote2, quote3]).to include(random_quote)
+    end
+
+  end
+
+  describe '#valid_total_length?' do
+
+    it 'delegates to TweetFormatter' do
+      tweet_formatter = double
+      TweetFormatter.stub(:new).and_return(tweet_formatter)
+      expect(tweet_formatter).to receive(:valid_length?)
+
+      quote = Quote.new(author: "author", text: "text")
+      expect(quote.valid?).to be_true
     end
 
   end
