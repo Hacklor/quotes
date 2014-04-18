3bb5eb5937c6ad734c5f5a23d85d5fe277434630
Nicole Bekkers <nicole@finalist.nl>
2014-04-18 10:58:52 +0200

    Error handling in twitter service

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -1,12 +1,18 @@
 class TwitterService
 
-  attr_reader :client
+  attr_reader :client, :error_message
   def initialize(client = TwitterClientFactory.get)
     @client = client
   end
 
   def tweet(text)
-    client.update(text)
+    begin
+      client.update(text)
+      true
+    rescue Twitter::Error => error
+      @error_message = error.message
+      false
+    end
   end
 
 end
