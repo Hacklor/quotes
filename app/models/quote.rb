822351603f208f8f18464b85f410f47ba420f9d0
Nicole Bekkers <nicole@finalist.nl>
2014-04-25 14:19:01 +0200

    Remove validation for length of quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,20 +1,10 @@
 class Quote < ActiveRecord::Base
 
   validates :author, :text, presence: true
-  validate :valid_total_length?
 
   def self.random
     offset = rand(Quote.count)
     Quote.offset(offset).first
-  end
-
-  private
-
-  def valid_total_length?
-    tweet_formatter = TweetFormatter.new(self)
-    unless tweet_formatter.valid_length?
-      errors[:total_length] = "can't be more than 140 characters"
-    end
   end
 
 end
