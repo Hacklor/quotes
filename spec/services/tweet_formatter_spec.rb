10fc89a8fb259d9fa51c306e3e9b61d074429998
Nicole Bekkers <nicole@finalist.nl>
2014-06-24 10:25:00 +0200

    Cut off a quote when it becomes too long

--- spec/services/tweet_formatter_spec.rb
+++ spec/services/tweet_formatter_spec.rb
@@ -4,43 +4,24 @@ describe TweetFormatter do
 
   describe '#formatted' do
 
-    let!(:quote) { double(author: "Albert Einstein",
-                          text: "Insanity: doing the same thing over and over again and expecting different results.") }
+    subject(:formatter) { TweetFormatter.new(quote) }
 
-    it 'formats the tweet' do
-      formatter = TweetFormatter.new(quote)
-      expect(formatter.formatted).to eq("\"#{quote.text}\" - #{quote.author}")
+    context 'short quote' do
+      let(:quote) { double(author: 'Albert Einstein', text: 'Insanity: doing the same thing over and over again and expecting different results.') }
+
+      it 'formats the tweet' do
+        expect(formatter.formatted).to eq("\"#{quote.text}\" - #{quote.author}")
+      end
     end
 
-  end
+    context 'too long quote' do
+      let(:quote) { double(author: 'a' * 5, text: 'b' * 131) }
 
-  describe "#valid_length?" do
+      it 'truncates a long tweet' do
+        expected_text = 'b' * 127
 
-    let!(:quote) { double(author: "", text: "") }
-    let!(:formatter) { TweetFormatter.new(quote) }
-
-    context 'valid' do
-
-      it 'returns true with length of 139 characters' do
-        5.times { quote.author << "a" }
-        129.times { quote.text << "b" }
-        expect(formatter.valid_length?).to be_true
-      end
-
-      it 'returns true with length of 140 characters' do
-        5.times { quote.author << "a" }
-        130.times { quote.text << "b" }
-        expect(formatter.valid_length?).to be_true
-      end
-
-    end
-
-    context 'invalid' do
-
-      it 'returns false with length of 141 characters' do
-        5.times { quote.author << "a" }
-        131.times { quote.text << "b" }
-        expect(formatter.valid_length?).to be_false
+        expect(formatter.formatted.length).to eq(140)
+        expect(formatter.formatted).to eq("\"#{expected_text}...\" - #{quote.author}")
       end
 
     end
